918c992dca61d78d8b143516efbd9f60973ff2d1
aashwinbhushal961 <aashwinbhushal961@outlook.com>
2023-05-20 20:45:03 +0530

    output binding: whenever the queue is triggered, use that msg and store in in b

--- queueTrigger1/queueTrigger.cs
+++ queueTrigger1/queueTrigger.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Text;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
@@ -8,14 +9,33 @@ namespace queueTrigger1
 {
     public class queueTrigger
     {
+        //#2 blob input binding :queue msg will be blob name
+
+        /* [FunctionName("queueTrigger")]
+         public void Run([QueueTrigger("myqueue-items", Connection = "AzureWebJobsStorage")] string myQueueItem,
+            //#2.
+            [Blob("dev/{queueTrigger}", FileAccess.Read, Connection = "AzureWebJobsStorage")] Stream s,
+           //#3.
+           ILogger log)
+         {
+             StreamReader reader = new StreamReader(s);
+             log.LogInformation($"C# Queue trigger function processed: \n name:{myQueueItem}  \n size:{s.Length} \n content:{reader.ReadToEnd()}");
+         }*/
+
+        //#3 output binding: whenever the queue is triggered, use that msg and store in in blob.
+        //video 22 of https://www.youtube.com/watch?v=XVWPabJUjt4&list=PLMWaZteqtEaLRsSynAsaS_aLzDPBUU4CV&index=22&t=324s 
+        
         [FunctionName("queueTrigger")]
-        public void Run([QueueTrigger("myqueue-items", Connection = "AzureWebJobsStorage")]string myQueueItem,
-            //blob input binding :queue msg will be blob name
-            [Blob("dev/{queueTrigger}",FileAccess.Read,Connection="AzureWebJobsStorage")] Stream s ,
-            ILogger log)
+        public void Run([QueueTrigger("myqueue-items", Connection = "AzureWebJobsStorage")] string myQueueItem,
+           //#3.
+           [Blob("dev/latestText.txt", FileAccess.Write, Connection = "AzureWebJobsStorage")] Stream outBlob,
+           ILogger log)
         {
-            StreamReader reader = new StreamReader(s);
-            log.LogInformation($"C# Queue trigger function processed: \n name:{myQueueItem}  \n size:{s.Length} \n content:{reader.ReadToEnd()}");
+            outBlob.Write(Encoding.ASCII.GetBytes(myQueueItem));
+            log.LogInformation($"C# Queue trigger function processed: \n name:{myQueueItem}  \n size:{outBlob.Length} \n content:");
         }
+
+
+
     }
 }
